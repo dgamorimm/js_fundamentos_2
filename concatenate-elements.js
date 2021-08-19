df627b7a7637f377b8a5c2c94d55e3a2909c8fab
douglasamorim-house <douglas@gavea.com>
2021-08-19 18:12:24 -0300

    add concatenate and list with two dimensions

---
+++ concatenate-elements.js
@@ -0,0 +1,6 @@
+const salaDePython = ['Melissa', 'Helena', 'Rodrigo']
+const salaDeJavaScript = ['Ju', 'Leo', 'Raquel']
+// une as duas turmas
+const salasUnificada = salaDePython.concat(salaDeJavaScript)
+
+console.log(salasUnificada);
